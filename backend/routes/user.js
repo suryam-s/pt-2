699723038bd0fbb723e9402635634735f91effb5
Suryam2048 <suryamsourya8@gmail.com>
2023-03-22 16:49:30 +0530

    controller in backend and dotenv

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,7 @@
 const express =require("express");
-
+const { home } = require("../controllers/user");
 const router =express.Router();
 
-router.get("/user",(req,res)=> {
-    res.send("welcome from user home");
-});
+router.get("/user", home);
+
 module.exports = router;
